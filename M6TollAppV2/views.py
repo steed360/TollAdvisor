e1717dd979b3ae8200637931488f5ed9b9f94a17
John Steedman <john_steedman@yahoo.com>
2013-06-11 13:54:56 +0100

    search functionality

--- M6TollAppV2/views.py
+++ M6TollAppV2/views.py
@@ -16,15 +16,17 @@ def init (request):
     return HttpResponse ("init done")
 
 def showMap (request):
+
     print settings.STATIC_ROOT
     print "---------------------------"
     print "STATIC_URL is " + settings.STATIC_URL
     print "---------------------------"
-
     return render_to_response ( "M6.html",{"server":request.META['HTTP_HOST']})
 
 
 def getRoute (request, fromX, fromY, toX, toY ):
+
+    print "in get route : %s, %s " %(fromX, toX)
 
     JSON_Result  =  RoutingFacade.findRoute (fromX, fromY, toX, toY )
     return HttpResponse (JSON_Result)
